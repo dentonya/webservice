b2f69d839e6bd778e754cbed4b6fc74ea641c7c4
dentonya <denatonya@gmail.com>
2024-02-23 08:06:36 +0300

    adding key used for project to settings.py file

--- webservice/webservice/settings.py
+++ webservice/webservice/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# add this key to your .env file 'django-insecure-+waw1x@qqrexdvqzxafx#op$4_q%1k9+fn92h5_-w@(5(46c^h'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
